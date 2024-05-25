564bf657197a5ff613c4ae885f47e0b67e672cd9
kdandy <50922642+kdandy@users.noreply.github.com>
2024-05-26 00:22:08 +0700

    Update command.rs

--- crates/help_telegram/src/command.rs
+++ crates/help_telegram/src/command.rs
@@ -3,7 +3,7 @@ use teloxide::utils::command::BotCommands;
 #[derive(BotCommands, Clone)]
 #[command(rename_rule = "lowercase", description = "These commands are supported:")]
 pub enum Command {
-    #[command(description = "Tanya apapun ke pembantu")]
+    #[command(description = "Tanya apapun ke aku")]
     Ask,
     #[command(description = "Bantuan menggunakan bot ini")]
     Help,
